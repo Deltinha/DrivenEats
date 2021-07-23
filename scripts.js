8ad69069d1c9d786b6724a6fa86cda7a1643eabd
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-07-23 20:21:25 -0300

    Backup

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function pickOption(){
+    document.querySelector("#meal-one").classList.toggle("food__card__green-border"); //Toggles green-border on selected card
+
+    document.querySelector("#meal-one ion-icon").classList.toggle("checkmark-hidden");//Toggles green-checkmark on selected card
+
+    
+}
