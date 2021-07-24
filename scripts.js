89308b10a1c1e65cbd818c493aa0f4c02f5f563f
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-07-24 15:37:57 -0300

    Pick only one of each food type requirement finished

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-function pickOption(){
-    document.querySelector("#meal-one").classList.toggle("food__card__green-border"); //Toggles green-border on selected card
+function pickCard(card, optionType){
+    const checkedCard = document.querySelector("." + optionType + " " + ".food__card__green-border"); //Look for a checked card in the respective group of cards.
+    if (checkedCard !== null){
+        checkedCard.classList.remove("food__card__green-border")
+        checkedCard.querySelector("ion-icon").classList.add("checkmark-hidden");
+    }
 
-    document.querySelector("#meal-one ion-icon").classList.toggle("checkmark-hidden");//Toggles green-checkmark on selected card
+    card.classList.add("food__card__green-border")                       //Toggles green-border on selected card
 
-    
+    card.querySelector("ion-icon").classList.remove("checkmark-hidden");    //Toggles green-checkmark on selected card
+
+    //função para verificar se as 3 opções estão selectionadas aqui
+    //função para habilitar place order aqui
 }
